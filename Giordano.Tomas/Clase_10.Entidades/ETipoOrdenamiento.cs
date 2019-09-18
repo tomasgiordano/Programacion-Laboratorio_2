9c6a579477c7f577300fb69a10a319028655f00f
alumno <alumno@utnfra.com.ar>
2019-09-18 11:59:35 -0300

    asd

---
+++ Giordano.Tomas/Clase_10.Entidades/ETipoOrdenamiento.cs
@@ -0,0 +1,7 @@
+﻿enum ETipoOrdenamiento
+{
+    LegajoAscendente,
+    LegajoDescendente,
+    ApellidoAscendente,
+    ApellidoDescendente,
+}
---
+++ Giordano.Tomas/Clase_10/FrmAlumno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_10
+{
+    public partial class FrmAlumno : Form
+    {
+        public FrmAlumno()
+        {
+            InitializeComponent();
+            foreach(ETipoExamen a in E)
+            {
+
+            }
+        }
+
+    }
+}
