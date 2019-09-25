29eb99539f85b49905652aab404a6c15c09c1181
alumno <alumno@utnfra.com.ar>
2019-09-25 11:58:58 -0300

    Asd

---
+++ CentralTelefonica/CentralitaPolimorfismo_entidades/Llamada.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CentralitaPolimorfismo_entidades
+{
+    public abstract class Llamada
+    {
+        protected float _duracion;
+        protected string _nroDestino;
+        protected string _nroOrigen;
+
+        public abstract float CostoLLamada{ get; }
+
+        public float Duracion
+        {
+            get { return this._duracion; }
+        }
+        public string NroDestino
+        {
+            get { return this._nroDestino; }
+        }
+        public string NroOrigen
+        {
+            get { return this._nroOrigen; }
+        }
+
+        public Llamada(string origen, string destino, float duracion)
+        {
+            this._nroOrigen = origen;
+            this._nroDestino = destino;
+            this._duracion = duracion;
+        }
+            
+        protected virtual string Mostrar()
+        {
+            return this._nroOrigen.ToString() + " " + this._nroDestino.ToString() + " " + this._duracion.ToString();
+        }
+
+        public static bool operator ==(Llamada uno,Llamada dos)
+        {
+            bool aux = false;
+            if(uno._nroDestino==dos._nroDestino&&uno._nroOrigen==dos._nroOrigen)
+            {
+                aux = true;
+            }
+
+            return aux;
+        }
+
+        public static bool operator !=(Llamada uno, Llamada dos)
+        {
+
+            return !(uno == dos);
+        }
+
+        public int OrdenarPorLlamada(Llamada uno, Llamada dos)
+        {
+            
+            if(uno._duracion>dos._duracion)
+            {
+                return 1;
+            }else if(uno._duracion < dos._duracion)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ CentralTelefonica/CentralitaPolimorfismo_entidades/Provincial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CentralitaPolimorfismo_entidades
+{
+    public class Provincial : Llamada
+    {
+        protected Franja _franjaHoraria;
+
+        public override float CostoLLamada{ get; }
+
+        public float Duracion
+        {
+            get;
+        }
+
+        private float CalcularCosto()
+        {
+
+        }
+
+        public bool Equals(object obj)
+        {
+
+        }
+
+        protected override string Mostrar()
+        {
+            return base.Mostrar()+" "+this._franjaHoraria.ToString();
+        }
+
+        public Provincial(Franja miFranja, Llamada unallamada):this(unallamada.NroOrigen,miFranja,unallamada.Duracion,unallamada.NroDestino)
+        {
+            
+        }
+
+        public Provincial(string origen,Franja miFranja,float duracion,string destino):base(origen, destino, duracion)
+        {
+            this._franjaHoraria = miFranja;
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+    }
+}
