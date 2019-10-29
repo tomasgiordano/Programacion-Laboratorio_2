ebc865457f87ccd3dabc0aa663f71153d77485af
alumno <alumno@utnfra.com.ar>
2019-10-29 11:42:03 -0300

    asd

---
+++ Giordano.Tomas/Clase_29-10/Test/Alumno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Alumno:Persona
+    {
+        public double nota;
+
+        public Alumno():this("","",-1,-1)
+        {
+
+        }
+        
+        public Alumno(string nombre,string apellido,int edad,double nota):base(nombre, apellido, edad)
+        {
+            this.nota = nota;
+        }
+        public override string ToString()
+        {
+            return base.ToString()+" "+this.nota;
+        }
+    }
+}
---
+++ Giordano.Tomas/Clase_29-10/Test/Empleado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Empleado:Persona
+    {
+        public int legajo;
+        public double sueldo;
+
+        public override string ToString()
+        {
+            return base.ToString()+" "+this.sueldo+" "+this.legajo;
+        }
+
+        public Empleado():this("", "", -1, -1,-1)
+        {
+
+        }
+
+        public Empleado(string nombre, string apellido, int edad, double sueldo,int legajo):base(nombre, apellido, edad)
+        {
+            this.sueldo = sueldo;
+            this.legajo = legajo;
+        }
+    }
+}
